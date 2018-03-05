c18e1ceda0cd4ae316f13a6cb2091f365a3c55db
doctorpacket <gottingerfrank@zoho.com>
2018-03-05 01:16:56 +0100

    added few basic programs

---
+++ numtest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int number;
+int main()
+{
+  puts("Please enter a number: ");
+  scanf(%d, number);
+  printf("The number you entered is: %i\n", number);
+}
+
