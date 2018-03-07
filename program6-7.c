848a59b9ffc13006a02c22753fef93974aabf27a
doctorpacket <gottingerfrank@zoho.com>
2018-03-07 23:59:00 +0100

    add 7-9

--- program6-7.c
+++ program6-7.c
@@ -5,44 +5,44 @@
 
 int main (void)
 {
-  const char baseDigits[16] = {
+    const char baseDigits[16] = {
     '0', '1', '2', '3',
     '4', '5', '6', '7',
     '8', '9', 'A', 'B',
     'C', 'D', 'E', 'F'
-  };
-  
-  int convertedNumber[64];
-  long int numberToConvert;
-  int nextDigit, base, index = 0;
+    };
 
-  // get the number and the base
+    int convertedNumber[64];
+    long int numberToConvert;
+    int nextDigit, base, index = 0;
 
-  printf ("Number to be converted? ");
-  scanf ("%ld", &numberToConvert);
+    // get the number and the base
 
-  printf ("Base? ");
-  scanf ("%i", &base);
+    printf ("Number to be converted? ");
+    scanf ("%ld", &numberToConvert);
 
-  // convert to the indicated base
+    printf ("Base? ");
+    scanf ("%i", &base);
 
-  do {
-    convertedNumber[index] = numberToConvert % base;
-    ++ index;
-    numberToConvert = numberToConvert / base;
-  }
-  while (numberToConvert != 0);
+    // convert to the indicated base
 
-  // display the results in reverse order
+    do {
+        convertedNumber[index] = numberToConvert % base;
+        ++ index;
+        numberToConvert = numberToConvert / base;
+        }
+    while (numberToConvert != 0);
 
-  printf ("Converted Number = ");
+    // display the results in reverse order
 
-  for (--index; index >= 0; --index) {
-    nextDigit = convertedNumber[index];
-    printf ("%c", baseDigits[nextDigit]);
-  }
+    printf ("Converted Number = ");
 
-  printf ("\n");
-  return 0;
+    for (--index; index >= 0; --index) {
+        nextDigit = convertedNumber[index];
+        printf ("%c", baseDigits[nextDigit]);
+    }
+
+    printf ("\n");
+    return 0;
 
 }
