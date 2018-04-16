f5fcf88669c77eb424e4365aa86125fc89f18d5f
doctorpacket <gottingerfrank@zoho.com>
2018-04-17 00:36:51 +0200

    Add 15.4 15.5

---
+++ program15-4.c
@@ -0,0 +1,16 @@
+// Program to echo characters
+// until an end of file
+
+#include <stdio.h>
+
+int main(void)
+{
+    int c;
+
+    while ( (c = getchar()) != EOF )
+        putchar(c);
+
+    return 0;
+
+}
+
